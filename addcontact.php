365b622b13d69eb73f2713bbe743876f494fad12
rtrave <>
2012-07-14 01:55:40 +0000

    Duplicate entries permit the contact edition.

--- addcontact.php
+++ addcontact.php
@@ -21,12 +21,14 @@ echo '
 if($app->dbmf_req_wcheck!='') {
 
     while($data_wcheck = MySBDB::fetch_array($app->dbmf_req_wcheck)) {
-        echo $data_wcheck[0].' <a href="modif.php?id='.$data_wcheck['0'].'&amp;mode=screen" ONCLICK="editwinopen(); return true;" TARGET="editwin"><b>'.$data_wcheck[1].'</b></a> '.$data_wcheck[2].'<br>';
+        echo '
+    '._G('DBMF_addcontact_editentry').$data_wcheck['id'].': 
+    <a href="javascript:editwinopen(\'index_wom.php?mod=dbmf3&amp;tpl=editcontact&amp;contact_id='.$data_wcheck['id'].'\',\'contactinfos\')"><b>'.$data_wcheck['lastname'].'</b> '.$data_wcheck['firstname'].'</a><br>';
     }
     echo '
-<br>
-<a href="index.php?mod=dbmf3&amp;tpl=editcontact&amp;contact_id=-1&amp;lastname='.$_POST['lastname'].'&amp;firstname='.$_POST['firstname'].'"><b>'._G('DBMF_addcontact_newentry').'</b></a>
-(<b>'.$_POST['lastname'].'</b> '.$_POST['firstname'].')<br>
+    <br>
+    '._G('DBMF_addcontact_newentry').': 
+    <a href="index.php?mod=dbmf3&amp;tpl=editcontact&amp;contact_id=-1&amp;lastname='.$_POST['lastname'].'&amp;firstname='.$_POST['firstname'].'"><b>'.$_POST['lastname'].'</b> '.$_POST['firstname'].'</a><br>
 </p>';
 
 } else {
