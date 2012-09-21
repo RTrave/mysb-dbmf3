fe7a51cbd946e49e8db859a64557b80c97863c8a
rtrave <>
2012-09-21 08:19:39 +0000

    Advanced search removed, in favour of display export.

--- request.php
+++ request.php
@@ -18,7 +18,24 @@ if(!MySBRoleHelper::checkAccess('dbmf_user')) return;
 
 echo '
 <h1>'._G('DBMF_request_title').'</h1>
+';
 
+if(isset($_POST['dbmf_request']) or isset($_POST['dbmf_request_advanced'])) {
+    echo '
+<h2>'._G('DBMF_search_results').'</h2>
+<p>
+'.MySBDB::num_rows($app->dbmf_search_result).' results<br>
+</p>
+';
+
+    $app->tpl_dbmf_searchresult = $app->dbmf_search_result;
+    _T('templates/contacts_display.php','dbmf3');
+
+    echo '
+';
+}
+
+echo '
 <h2>'._G('DBMF_search').'</h2>
 
 <h3>'._G('DBMF_search_keyword').'</h3>
@@ -40,90 +57,5 @@ echo '
 </form>
 ';
 
-echo '
-<h3>'._G('DBMF_search_advanced').'</h3>
-
-<form action="" method="post">
-<div class="table_support" align="center">
-<table><tbody>
-
-';
-
-$blocks = MySBDBMFBlockHelper::load();
-$blockn_flag = 0;
-foreach($blocks as $block) {
-    $group_edit = MySBGroupHelper::getByID($block->groupedit_id);
-    if($blockn_flag==0) $blockn_flag = 1;
-    elseif($block->isEditable()) {
-        echo '
-<tr>
-    <td colspan="2" style="text-align: center;">
-    <select name="block_andorflag_'.$block->id.'">
-        <option value="or">OR</option>
-        <option value="and">AND</option>
-    </select>
-    </td>
-</tr>';
-    }
-    echo '
-<tr class="title" >
-    <td colspan="2">';
-    if($block->isEditable()) 
-        echo $block->htmlFormWhereClause('b').' ';
-    echo $block->lname.' <small><i>('.$group_edit->comments.')</i></small></td>
-</tr>';
-    if($block->isEditable()) {
-        echo '
-<tr>
-    <td style="text-align: right;">'._G('DBMF_request_blockref_and_or').'</td>
-    <td>
-    <select name="blockref_andorflag_'.$block->id.'">
-        <option value="or">OR</option>
-        <option value="and">AND</option>
-    </select>
-    </td>
-</tr>';
-    }
-    foreach($block->blockrefs as $blockref) {
-        if($block->isEditable() and $blockref->isActive()) {
-            $refname = 'br'.$blockref->id;
-            echo '
-<tr style="'.$class_edit.'">
-    <td style="vertical-align: top; text-align: right;"><b>'.$blockref->lname.':</b></td>
-    <td>';
-            echo $blockref->htmlFormWhereClause('br',$contact->$refname);
-            echo '
-    </td>
-</tr>';
-        }
-    }
-}
-
-echo '
-<tr>
-    <td colspan="2" align="center">
-        <input type="hidden" name="dbmf_request_advanced" value="1">
-        <input type="submit" value="'._G('DBMF_search_submit').'" class="submit">
-    </td>
-</tr>
-</tbody></table>
-</div>
-</form>
-';
-
-if(isset($_POST['dbmf_request']) or isset($_POST['dbmf_request_advanced'])) {
-    echo '
-<h2>'._G('DBMF_search_results').'</h2>
-<p>
-'.MySBDB::num_rows($app->dbmf_search_result).' results<br>
-</p>
-';
-
-    $app->tpl_dbmf_searchresult = $app->dbmf_search_result;
-    _T('templates/contacts_display.php','dbmf3');
-
-    echo '
-';
-}
 
 ?>
--- request_process.php
+++ request_process.php
@@ -16,57 +16,37 @@ global $app;
 
 if(isset($_POST['dbmf_request'])) {
 
+    $sql_r = 'SELECT * from '.MySB_DBPREFIX.'dbmfcontacts ';
+    $clause_a = '';
+    $blocks = MySBDBMFBlockHelper::load();
+    $clause_owner = '';
+    foreach($blocks as $block) {
+        if($block->isEditable()) {
+            if($clause_owner!='')  $clause_owner .= ' or ';
+            $clause_owner .= $block->htmlProcessWhereClause();
+        }
+    }
     if (!empty($_POST['search_all'])) {
         $str_search_all = MySBUtil::str2whereclause($_POST['search_all']);
-   	    $sql_r = 'SELECT * from '.MySB_DBPREFIX.'dbmfcontacts '.
-        'WHERE lastname RLIKE \''.$str_search_all.'\' OR '.
-   	    'firstname RLIKE \''.$str_search_all.'\' OR '.
-   	    'function RLIKE \''.$str_search_all.'\' OR '.
-   	    'organism RLIKE \''.$str_search_all.'\' OR '.
-   	    'comments LIKE \''.$str_search_all.'\' OR '.
-   	    'mail RLIKE \''.$str_search_all.'\' '.
-   	    'ORDER by lastname;';
+   	    $clause_a = 'lastname RLIKE \''.$str_search_all.'\' OR '.
+   	        'firstname RLIKE \''.$str_search_all.'\' OR '.
+   	        'function RLIKE \''.$str_search_all.'\' OR '.
+   	        'organism RLIKE \''.$str_search_all.'\' OR '.
+   	        'comments LIKE \''.$str_search_all.'\' OR '.
+   	        'mail RLIKE \''.$str_search_all.'\'';
    	    $_POST['search_name'] = '';
     } elseif(!empty($_POST['search_name'])) {
-        $sql_r = 'SELECT * from '.MySB_DBPREFIX.'dbmfcontacts '.
-        'WHERE lastname RLIKE \''.MySBUtil::str2whereclause($_POST['search_name']).'\' '.
-        'ORDER by lastname;';
+        $clause_a = 'lastname RLIKE \''.MySBUtil::str2whereclause($_POST['search_name']).'\' ';
     } else {
-        $sql_r = 'SELECT * from '.MySB_DBPREFIX.'dbmfcontacts '.
-        'ORDER by lastname;';
+        $clause_a = '';
     }
+    if($clause_owner!='' and $clause_a!='') $clause_a .= ' and ('.$clause_owner.')';
+    elseif($clause_owner!='') $clause_a .= '('.$clause_owner.')';
+    if($clause_a!='') $sql_r .= 'WHERE '.$clause_a.' ';
+    $sql_r .= 'ORDER by lastname';
 	$app->dbmf_search_result = MySBDB::query( $sql_r,
 	    "request_process.php",
 	    false, 'dbmf3');
-}
-
-if(isset($_POST['dbmf_request_advanced'])) {
-
-    $sql_a = 'SELECT * from '.MySB_DBPREFIX.'dbmfcontacts ';
-    $clause_a = '';
-    $blocks = MySBDBMFBlockHelper::load();
-    foreach($blocks as $block) {
-        $group_edit = MySBGroupHelper::getByID($block->groupedit_id);
-        if(($clause=$block->htmlProcessWhereClause('b'))=='') {
-            foreach($block->blockrefs as $blockref) {
-                if($block->isEditable() and $blockref->isActive()) {
-                    $refname = 'br'.$blockref->id;
-                    if(($clause_t = $blockref->htmlProcessWhereClause('br'))!=null) {
-                        if($clause!='') $clause .= ' '.$_POST['blockref_andorflag_'.$block->id].' ';
-                        $clause .= $clause_t;
-                    }
-                }
-            }
-        }
-        if($clause_a!='' and $clause!='') $clause_a .= ' '.$_POST['block_andorflag_'.$block->id].' ';
-        if($clause!='') $clause_a .= '('.$clause.')';
-    }
-    if($clause_a!='') $sql_a .= 'WHERE '.$clause_a.' ';
-    $sql_a .= 'ORDER by lastname';
-	$app->dbmf_search_result = MySBDB::query( $sql_a,
-	    "request_process.php",
-	    false, 'dbmf3');
-
 }
 
 ?>
